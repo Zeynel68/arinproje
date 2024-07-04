5c0db801c632bd00e43d1209da4cb651a71d5567
Zeynel Hüseyni <huseynizeynel@gmail.com>
2024-07-05 00:53:18 +0300

    Proje şimdilik bitti

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"
+  content: ["./src/**/*.{html,js}",
+  darkMode,"class"
   ],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+    },
+    extend: {
+    fontFamily:{
+      gemunu:["Gemunu Libre", "sans-serif"],
+      open:["Open Sans", "sans-serif"]
+    },
+    colors:{
+      "arin-red":"#BC1A45",
+    "arin-melon":"#FFD369",
+    "arin-grey" :"#DDDDDD",
+    "arin-white":"#F7F7F7"
+    },
+    spacing:{
+      128: "32 rem"
+    }
+    },
   },
   plugins: [],
 }
